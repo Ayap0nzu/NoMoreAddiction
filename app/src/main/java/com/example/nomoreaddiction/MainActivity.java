54f7fc04f2b096f80bfd9369bab563a012329648
Name <you@example.com>
2023-04-09 19:24:08 +0900

    Add notification

--- app/src/main/java/com/example/nomoreaddiction/MainActivity.java
+++ app/src/main/java/com/example/nomoreaddiction/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.nomoreaddiction;
 
-
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
@@ -9,15 +7,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
+import android.os.Handler;
 import android.os.SystemClock;
-import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Chronometer;
+
+import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -27,23 +32,28 @@ public class MainActivity extends AppCompatActivity{
     private static Chronometer chronometer;
     private long pauseOffset = 0;
     private boolean running;
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ;
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd (EEE)", Locale.KOREA);
         String formattedDate = sdf.format(new Date());
-
         TextView textView = (TextView) findViewById(R.id.DATE);
         textView.setText(formattedDate);
 
-        //스탑워치
+        //      스탑워치
         chronometer = findViewById(R.id.chronometer);
-        //  앱을 처음 실행 시켰을때 유저에게 앱 소개.
+
+        //      휴대폰을 얼마나 사용했는지 보여주는 알림 창
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        //      앱을 처음 실행 시켰을때 유저에게 앱 소개.
         final String PREFS_NAME = "MyPrefsFile";
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         if (settings.getBoolean("my_first_time", true)) {
@@ -57,45 +67,93 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-    //onResume, onPause 함수 지우면 스탑워치가 안돌아가니 안지우게 주의
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(!running){
-            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-            chronometer.start();
-            running = true;
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(running){
-            chronometer.stop();
-            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
-            running = false;
-        }
-    }
-
-    public class MyReceiver extends BroadcastReceiver {
+    //onResume 함수 지우면 스탑워치가 안돌아가니 안지우게 주의
+    private BroadcastReceiver screenOffReceiver = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context arg0, Intent arg1) {
-            // TODO Auto-generated method stub
-            Log.v("#@%@%#", "Power button is pressed.");
-            Toast.makeText(arg0, "power button clicked",Toast.LENGTH_LONG).show();
-            //perform what you want here
-            if(!running){
-                chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
-                chronometer.start();
-                running = true;
-            }
-            if(running){
+        public void onReceive(Context context, Intent intent) {
+            //  만약 휴대폰 스크린이 꺼지면 스탑워치를 멈춘다.
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 chronometer.stop();
                 pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
                 running = false;
             }
         }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        long chronometerTime = SystemClock.elapsedRealtime() - chronometer.getBase();
+        // Save the current chronometer time to SharedPreferences
+        SharedPreferences prefs = getSharedPreferences("mySharedPrefsFilename", Context.MODE_PRIVATE);
+
+        // Don't forget to call commit() when changing preferences.
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putLong("chronoValue", chronometerTime);
+
+        String currentDate = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(new Date());
+        editor.putString("currentDate", currentDate);
+
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getSharedPreferences("mySharedPrefsFilename", Context.MODE_PRIVATE);
+        long chronometerTime = prefs.getLong("chronoValue", 0);
+        String storedDate = prefs.getString("currentDate", "");
+
+        // Get the current date
+        String currentDate = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(new Date());
+
+        //  만약 하루가 지났으면
+        if (!storedDate.equals(currentDate)) {
+            chronometer.setBase(SystemClock.elapsedRealtime());
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putLong("chronoValue", chronometerTime);
+            editor.apply();
+        }
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenOffReceiver, filter);
+
+
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - chronometerTime);
+            chronometer.start();
+            running = true;
+
+            Handler handler = new Handler();
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    long currentTime = SystemClock.elapsedRealtime() - chronometer.getBase();
+                    int elapsedHours = (int) (currentTime / 3600000);
+                    int elapsedMinutes = (int) ((currentTime / 60000) % 60);
+                    int elapsedSeconds = (int) ((currentTime / 1000) % 60);
+
+                    // Format the elapsed time as a string in "HH:MM:SS" format
+                    String formattedTime = String.format("%02d:%02d:%02d", elapsedHours, elapsedMinutes, elapsedSeconds);
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, "My Notification");
+                    builder.setContentTitle("app");
+                    builder.setContentText(formattedTime);
+                    builder.setSmallIcon(R.drawable.ic_launcher_background);
+                    builder.setAutoCancel(true);
+
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainActivity.this);
+                    managerCompat.notify(1, builder.build());
+
+                    // Schedule the next update after 5 seconds
+                    handler.postDelayed(this, 5000);
+                }
+            };
+
+            // Schedule the first update after 5 seconds
+            handler.postDelayed(runnable, 5000);
+        }
     }
 }
+
+
